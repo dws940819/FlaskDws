dab203d9fd3fb820b77e7276cbc1fba14122b2e2
dws <695036039@qq.com>
2020-04-01 16:39:48 +0800

    '第一个项目和环境变量配置'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -2,6 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+
+@app.route('/dws')
 def index():
     return "<h1>Flask，Dws来了！</h1>"
+
+# 动态URL
+@app.route('/index/<name>')
+def home(name):
+    return '<h1>Flask，%s来了！</h1>'% name
 
