bf4fac784bf1814203613dc6ae374f3f209b491d
dws <695036039@qq.com>
2020-04-01 17:22:18 +0800

    '首页模板完成'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-
-@app.route('/dws')
 def index():
-    return "<h1>Flask，Dws来了！</h1>"
+    name = 'Dws'
+    movies = [
+        {'title':'唐探3','year':'2020'},
+        {'title':'囧妈','year':'2020'},
+        {'title':'缝纫机乐队','year':'2020'},
+        {'title':'我的青春期','year':'2020'},
+        {'title':'反转人生','year':'2020'},
+        {'title':'大千世界','year':'2020'},
+    ]
+    # return "<h1>Flask，Dws来了！</h1>"
+    return render_template('index.html', name=name, movies=movies)
 
-# 动态URL
-@app.route('/index/<name>')
-def home(name):
-    return '<h1>Flask，%s来了！</h1>'% name
+# # 动态URL
+# @app.route('/index/<name>')
+# def home(name):
+#     return '<h1>Flask，%s来了！</h1>'% name
 
